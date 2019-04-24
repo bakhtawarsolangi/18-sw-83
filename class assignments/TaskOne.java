575ab7748c5061cbc85aa5f6c2eee590cf256617
bakhtawarsolangi <49591438+bakhtawarsolangi@users.noreply.github.com>
2019-04-25 01:23:08 +0500

    Add files via upload

---
+++ class assignments/TaskOne.java
@@ -0,0 +1,11 @@
+class TaskOne{
+     public static void main(String args[])
+     {
+       for(int k=0;k<=40;k++){
+             if(k%2!=0){
+          continue;
+         }
+       System.out.print(k+" ");
+     }
+   }
+ }
---
+++ class assignments/TaskTwo.java
@@ -0,0 +1,11 @@
+class TaskTwo{
+   public static void main(String[]args)
+   {
+     for(int j=0;j<=40;j++){
+      if(j%2==0){
+      continue;
+     }
+    System.out.print(j+" ");
+      }
+   }
+}
